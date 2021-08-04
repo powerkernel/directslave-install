c743c1d776122c9289f21323e336af8f5e09aae2
Harry Tang <harry@powerkernel.com>
2021-08-04 22:12:29 +0300

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -28,10 +28,7 @@ touch /var/lib/bind/slave/directslave.inc
 
 # configure bind9
 echo "include \"/var/lib/bind/slave/directslave.inc\";" >> /etc/bind/named.conf
-sed -i "s/listen-on-v6 { any; };/#listen-on-v6 { any; };/g" /etc/bind/named.conf.options
-echo "listen-on port 53 { any; };" >> /etc/bind/named.conf.options
-echo "listen-on-v6 port 53 { any; };" >> /etc/bind/named.conf.options
-echo "allow-transfer {\"none\";};" >> /etc/bind/named.conf.options
+sed -i "s/listen-on-v6 { any; };#/listen-on-v6 { any; };\n        listen-on port 53 { any; };\n        listen-on-v6 port 53 { any; };\n        allow-transfer {\"none\";};/g" /etc/bind/named.conf.options
 
 # permission
 touch /usr/local/directslave/etc/passwd
